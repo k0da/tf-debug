d79ef187fadb03addcd328aee6b5f86a7cd2e2fd
Dinar Valeev <dinar.valeev@absa.africa>
2020-10-12 16:59:23 +0200

    Test new tf-controller
    Signed-off-by: Dinar Valeev <dinar.valeev@absa.africa>

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
-data "kubernetes_secret" "foo" {
-  metadata {
-    name = var.s_name
-    namespace = var.s_ns
-  }
+output "secret1" {
+ value = var.secret1 
+}
+output "secret2" {
+ value = var.secret2
+}
+output "envsecret1" {
+ value = var.envsecret1 
+}
+output "envsecret2" {
+ value = var.envsecret2
+}
+output "config1" {
+ value = var.config1 
+}
+output "config2" {
+ value = var.config2
+}
+output "envconfig1" {
+ value = var.envconfig1 
+}
+output "envconfig2" {
+ value = var.envconfig2
 }
 
-output "secret" {
- value = data.kubernetes_secret.foo.data.admin-creds
-}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,8 @@
-variable "s_name" {}
-variable "s_ns" {}
+variable "config1" {}
+variable "config2" {}
+variable "secret1" {}
+variable "secret2" {}
+variable "envconfig1" {}
+variable "envconfig2" {}
+variable "envsecret1" {}
+variable "envsecret2" {}
